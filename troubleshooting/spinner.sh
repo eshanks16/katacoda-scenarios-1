21c42dcff3665930885e32072bbe352870462928
Eric Shanks <eshanks@eshanks-a01.vmware.com>
2020-05-28 10:23:53 -0500

    Changing foreground intro test

--- troubleshooting/spinner.sh
+++ troubleshooting/spinner.sh
@@ -1,24 +1,2 @@
 #!/bin/bash
-
-show_progress()
-{
-  echo -n "Starting"
-  local -r pid="${1}"
-  local -r delay='0.75'
-  local spinstr='\|/-'
-  local temp
-  while true; do 
-    sudo grep -i "done" /root/katacoda-finished &> /dev/null
-    if [[ "$?" -ne 0 ]]; then     
-      temp="${spinstr#?}"
-      printf " [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep "${delay}"
-      printf "\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-}
-
-show_progress
+sleep 1; wait.sh
