27423a203d88efbf29fdf99ad68440c9a3b730ea
Eric Shanks <eshanks@eshanks-a01.vmware.com>
2020-07-01 14:00:20 -0500

    Fixing setup

--- troubleshooting-services/launch.sh
+++ troubleshooting-services/launch.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-while true; do
-    NUM_READY=`kubectl get nodes 2> /dev/null | grep -v NAME | awk '{print $2}' | grep -e ^Ready | wc -l`
-    if [ "${NUM_READY}" == "2" ]; then
-        echo "Ready" > /root/ready
-        break
-    else
-        echo "Waiting for all Kubernetes nodes to be ready. Current ready nodes: ${NUM_READY}"
-        kubectl get nodes
-    fi
-    sleep 
+#! /bin/bash
+docker pull gcr.io/kuar-demo/kuard-amd64:blue
--- troubleshooting-services/scenario1.sh
+++ troubleshooting-services/scenario1.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-docker pull gcr.io/kuar-demo/kuard-amd64:blue
 cd ~/manifests
 kubectl apply -f kuard-1.yaml
