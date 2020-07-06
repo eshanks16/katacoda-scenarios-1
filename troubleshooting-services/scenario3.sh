00c4b38d591b707702dd3eabc6b249aff417e7bd
Eric Shanks <eshanks@eshanks-a01.vmware.com>
2020-07-06 15:34:14 -0500

    Deleting curl pod from previous scenario

--- troubleshooting-services/scenario3.sh
+++ troubleshooting-services/scenario3.sh
@@ -2,6 +2,7 @@
 cd ~/manifests
 
 #kubectl apply -f scenario1-answer.yaml
+kubectl delete pod curl
 kubectl apply -f ../webserver-1.yaml
 kubectl get daemonset kube-proxy -n kube-system -o yaml > /root/manifests/kube-proxy.yaml
 kubectl delete daemonset kube-proxy -n kube-system
