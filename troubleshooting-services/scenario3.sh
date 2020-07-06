cf000d1a72ab253aaaa053b44c094c9746c000ce
Eric Shanks <eshanks@eshanks-a01.vmware.com>
2020-07-06 15:58:57 -0500

    changing deployment order

--- troubleshooting-services/scenario3.sh
+++ troubleshooting-services/scenario3.sh
@@ -2,7 +2,9 @@
 cd ~/manifests
 
 #kubectl apply -f scenario1-answer.yaml
-kubectl delete pod curl
-kubectl apply -f ../webserver-1.yaml
-kubectl get daemonset kube-proxy -n kube-system -o yaml > /root/manifests/kube-proxy.yaml
+kubectl delete -f ../webserver-1.yaml
+kubectl get daemonset kube-proxy -n kube-system -o yaml > /root/manifests/
+kube-proxy.yaml
 kubectl delete daemonset kube-proxy -n kube-system
+kubectl apply -f ../webserver-1.yaml
+kubectl delete pod curl
