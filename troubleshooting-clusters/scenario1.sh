1c87f8b9c531219fa905fa8871aedbbb6b019a5a
Eric Shanks <eshanks@eshanks-a01.vmware.com>
2020-07-17 13:18:54 -0500

    cleaning

--- troubleshooting-clusters/scenario1.sh
+++ troubleshooting-clusters/scenario1.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sed 's/--trusted-ca-file=\/etc\/kubernetes\/pki\/etcd\/ca.crt/--trusted-ca-file=\/etc\/kubernetes\/pki\/etcd\/wrongca.crt/' etcd.yaml -i
+sed 's/--trusted-ca-file=\/etc\/kubernetes\/pki\/etcd\/ca.crt/--trusted-ca-file=\/etc\/kubernetes\/pki\/etcd\/wrongca.crt/' /etc/kubernetes/manifests/etcd.yaml -i
